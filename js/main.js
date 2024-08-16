35288c985ec6f721d7d681c93b7f5ec05a861159
matsumoto.n <matsumoto.n@b-style.net>
2024-08-16 18:23:19 +0900

    ベーシック認証を追加

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+// main.js
+// 正しいパスワードを設定
+const correctPassword = 'yourpassword'; // パスワードを設定
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const loginDiv = document.getElementById('login');
+    const contentDiv = document.querySelector('.container');
+
+    loginForm.addEventListener('submit', function (e) {
+        e.preventDefault();
+        const password = document.getElementById('password').value;
+        if (password === correctPassword) {
+            loginDiv.style.display = 'none';
+            contentDiv.style.display = 'block';
+        } else {
+            alert('Incorrect password!');
+        }
+    });
+});
+
 function navigateTo(page) {
     window.location.href = page;
 }
